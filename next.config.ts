8c56b116dc1f408b656dd60bad2df7f6e40c6e1a
RomanSinAlexandra <154755002+RomanSinAlexandra@users.noreply.github.com>
2026-04-24 01:01:13 +0300

    Update basePath and assetPrefix for production
    Set basePath and assetPrefix based on production environment.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = '/Ryoko-Tsukiko-Bot-Site-';
+
 const nextConfig = {
-  output: 'export', 
-  basePath: '/Ryoko-Tsukiko-Bot-Site-',
+output: 'export',
+  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? repoName : '', // Добавляет префикс к JS/CSS файлам
   images: {
     unoptimized: true,
   },
